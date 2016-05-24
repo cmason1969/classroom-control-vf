c64b53c130353926ae7c0014aa5ae724018570a4
cmason1969 <chad.mason@outlook.com>
2016-05-24 09:53:54 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,13 @@ node default {
   # Example:
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
+  
+  file { '/etc/motd':
+  ensure  => file,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  content => "Today is the 24th of May 2016.\n",  }
+
+
 }
